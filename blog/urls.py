e5fdb98c0942e955956d845deaab1c7ffe45f83b
Akash N O <noakash@ymail.com>
2021-03-21 14:42:12 +0530

    about me

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ from blog.views import *
 urlpatterns = [
     path('', home, name='home'),
     path('post/<int:pk>', post, name='post'),
+    path('about/',about, name='about'),
+    path('about/resume/',resume, name='resume'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -10,18 +10,27 @@ from blog.models import Post, Section
 def home(request):
     posts = Post.objects.all()
     if request.POST:
-         item = request.POST.get('text')
-         posts = ModelFilter(model_objects=posts, item=item).filter()
-         posts = serializers.serialize("json", posts)
-         response = JsonResponse({'posts': posts})
-         return response
-    context = {'posts':posts}
+        item = request.POST.get('text')
+        posts = ModelFilter(model_objects=posts, item=item).filter()
+        posts = serializers.serialize("json", posts)
+        response = JsonResponse({'posts': posts})
+        return response
+    context = {'posts': posts}
     return render(request, 'blog/home.html', context)
 
 
-def post(request,pk):
+def post(request, pk):
     post = Post.objects.get(id=pk)
     sections = Section.objects.filter(post=post)
     share_string = quote_plus(post.title)
-    context = {'post':post, 'sections':sections,'share_string':share_string}
+    context = {'post': post, 'sections': sections, 'share_string': share_string}
     return render(request, 'blog/post.html', context)
+
+
+def about(request):
+    context = {}
+    return render(request, 'blog/about.html', context)
+
+
+def resume(request):
+    return render(request,'blog/resume.html')
