f48bbbe06210d54dc36d4d86965d013d11ced3ee
noakash <52347258+Akashno@users.noreply.github.com>
2021-07-31 21:21:52 +0530

    Update settings.py

--- nocodes/settings.py
+++ nocodes/settings.py
@@ -120,13 +120,21 @@ USE_L10N = True
 USE_TZ = True
 
 #cors_headers
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://localhost:8080",
+#     "https://nocodes-vue.herokuapp.com"
+
+# ]
+CORS_ORIGIN_ALLOW_ALL=True
+CORS_ORIGIN_WHITELIST = [
+    "https://example.com",
+    "https://sub.example.com",
     "http://localhost:8080",
+    "http://127.0.0.1:9000",
     "https://nocodes-vue.herokuapp.com"
-
+    
 ]
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
